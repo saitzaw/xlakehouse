5c8ea0285b3101918451c07a1c74cdab529900ff
saitzaw <saithihazaw43@gmail.com>
2024-11-10 18:22:08 +0700

    fix: missing business key

--- flight_tickets/models/raw_vault/satellites/s_seats.sql
+++ flight_tickets/models/raw_vault/satellites/s_seats.sql
@@ -2,13 +2,13 @@
     config(
         materialized = 'incremental'
         , schema = 'datavault2'
-        , unique_key = 
+        , unique_key = generate_md5([aircraft_code, seat_no])
     )
 }}
 
 with cte_s_seats as (
     select 
-        generate_md5()
+        generate_md5(generate_md5([aircraft_code, seat_no])) as hk_seat
         , fare_conditions
         , generate_current_time()
         , generate_source_table
