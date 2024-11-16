65b453ec10084f6b6f31bca4b6d6558c25ee107e
saitzaw <saithihazaw43@gmail.com>
2024-11-16 18:14:12 +0700

    add the ochestration scripts

---
+++ dags/includes/stg_flight_tickets/README.md
@@ -0,0 +1,13 @@
+### staging script to run 
+- integration with Airflow and dbt together 
+- incremental model
+
+### DBT list 
+- stg_aircrafts 
+- stg_airports
+- stg_boarding_passes 
+- stg_bookings
+- stg_flights
+- stg_seats
+- stg_ticket_flights
+- stg_tickets
---
+++ dags/includes/stg_flight_tickets/stg_bookings.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+MY_PATH=$HOME/projects/finops
+MY_SRC_FOLDER=$MY_PATH/flight_tickets
+
+# check the project is located in 
+if [ -d $MY_PATH ]; then 
+    cd $MY_PATH
+    source .dbt-1.8.8/bin/activate 
+    if [ $VIRTUAL_ENV != "" ]; then
+         cd $MY_SRC_FOLDER
+         dbt run --select models/staging/stg_bookings.sql
+         deactivate
+    else 
+        echo "VIRTUAL ENVRIONMENT CANNOT BE ACTIVATED: check in $MY_PATH" 
+    fi 
+else
+    echo "CANNOT FIND THE LOCATION: $MY_PATH is not found" 
+fi 
+
+
+
+
+
